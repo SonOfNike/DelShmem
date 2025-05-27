942a5ed9ba22abab05e42063a5ca63487626416a
SonOfNike <chrisfreeman.trades@gmail.com>
2025-05-27 17:35:15 +0000

    del shmem

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "../Utils/globals.h"
+
+int main() {
+    
+    // Remove the MD shared memory object (if this is the last process)
+    shm_unlink(MD_shm_name);
+
+    // Remove the Req shared memory object (if this is the last process)
+    shm_unlink(REQ_shm_name);
+
+    // Remove the Resp shared memory object (if this is the last process)
+    shm_unlink(RESP_shm_name);
+
+    return 0;
+}
